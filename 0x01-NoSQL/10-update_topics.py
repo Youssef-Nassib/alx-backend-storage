42720c6d30b27896a77d083522292c7b7694b609
Youssef-Nassib <77752532+Youssef-Nassib@users.noreply.github.com>
2024-10-23 00:29:26 +0100

    Update 10-update_topics.py

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -6,7 +6,6 @@ Change school topics
 
 def update_topics(mongo_collection, name, topics):
     """
-    changes all topics of a school
-     document
+    changes all topics of a school document
     """
     mongo_collection.update_many({"name": name}, {"$set": {"topics": topics}})
